3d0593dc84f8607a968ec5f7540c84823c01b4b5
elBichon <reuglewiczjeanedouard@gmail.com>
2021-02-21 00:11:35 +0100

    adding text processing and tfidf for search

--- utils.py
+++ utils.py
@@ -2,48 +2,10 @@ import re
 import spacy
 import flask
 from flask import Flask, render_template, request, flash, session
-from nltk.tokenize import word_tokenize
-import nltk
 import pandas as pd
-from gensim.parsing.preprocessing import remove_stopwords
-import gensim
-from gensim.models import Word2Vec
-import nltk
-from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-def clean_data(text):
-	try:
-		if len(str(text)) > 0 and isinstance(text, str) == True:
-			#nlp = en_core_web_sm.load()
-			nlp = spacy.load('en_core_web_sm')
-			nlp.max_length = 1500000
-			stemmer = SnowballStemmer(language='english')
-			text = remove_stopwords(text).lower()
-			text = (re.sub("[^a-zA-Z]"," ",text)).rstrip().lstrip()
-			return text
-		else:
-			return False
-	except:
-		return False
-
-
-def get_data(df,text,query):
-	try:
-		if len(str(query)) > 0 and isinstance(query, str) == True and len(str(text)) > 0 and isinstance(text, list) == True:
-			vectorizer = TfidfVectorizer()
-			X = vectorizer.fit_transform(text)
-			vectorizer.fit(text)
-			vector = vectorizer.transform([query])
-			results = cosine_similarity(X,vector).reshape((-1,))
-			df['grades'] = results
-			df = df.sort_values(by=['grades'], ascending=False)
-			return df[['title','recommendations','artist','publisher','writer','genres','summary']].head(100)
-		else:
-			return False
-	except:
-		return False
+import string
 
 
 def read_data(dataset):
@@ -65,13 +27,13 @@ def generate_research_choice(df, couleur, pays):
 		if couleur != 'no' and pays != 'no':
 			df = df.loc[df['couleur'] == couleur]
 			df = df.loc[df['pays'] == pays]
-			answer = df[['name', 'image', 'pays','region', 'appelation', 'domaine', 'millesime', 'couleur','description']].head(10)
+			answer = df[['name', 'image', 'pays','region', 'appelation', 'domaine', 'millesime', 'couleur','description','clean_text']].head(10)
 		elif couleur == 'no' and pays != 'no':
 			df = df.loc[df['pays'] == pays]
-			answer = df[['name', 'image', 'pays','region', 'appelation', 'domaine', 'millesime', 'couleur','description']].head(10)
+			answer = df[['name', 'image', 'pays','region', 'appelation', 'domaine', 'millesime', 'couleur','description','clean_text']].head(10)
 		elif couleur != 'no' and pays == 'no':
 			df = df.loc[df['couleur'] == couleur]
-			answer = df[['name', 'image', 'pays','region', 'appelation', 'domaine', 'millesime', 'couleur','description']].head(10)
+			answer = df[['name', 'image', 'pays','region', 'appelation', 'domaine', 'millesime', 'couleur','description','clean_text']].head(10)
 		elif couleur == 'no' and pays == 'no':
 			answer = 'void'
 		else:
@@ -93,3 +55,59 @@ def generate_answer(answer):
 	except:
 		return False
 
+def process_request(input_data):
+	try:
+		input_data = input_data.lower()
+		input_data = input_data.replace(r"[^A-Za-z0-9éèçàùâêî]", " ")
+		input_data = input_data.replace(" a ", " ")
+		input_data = input_data.replace(" d ", " ")
+		input_data = input_data.replace(" d' ", " ")
+		input_data = input_data.replace(" d'", " ")
+		input_data = input_data.replace(" l ", " ")
+		input_data = input_data.replace(" s ", " ")
+		input_data = input_data.replace(" c ", " ")
+		input_data = input_data.replace(" i ", " ")
+		input_data = input_data.replace(" y ", " ")
+		input_data = input_data.replace(" à ", " ")
+		input_data = input_data.replace(" n ", " ")
+		input_data = input_data.replace(" n' ", " ")
+		input_data = input_data.replace(" n'", " ")
+		input_data = input_data.translate(str.maketrans('', '', string.punctuation))
+		input_data = input_data.rstrip()
+		input_data = input_data.lstrip()
+		return input_data
+	except:
+		return False
+
+def remove_stop_words(input_data, STOP):
+	try:
+		querywords = input_data.split()
+		resultwords  = [word for word in querywords if word not in STOP]
+		result = ' '.join(resultwords)
+		return result
+	except:
+		return False
+
+def get_data(df,column,query):
+	try:
+		vectorizer = TfidfVectorizer()
+		X = vectorizer.fit_transform(df[column].values.tolist())
+		print('a')
+		vectorizer.fit(df[column].values.tolist())
+		vector = vectorizer.transform([query])
+		results = cosine_similarity(X,vector).reshape((-1,))
+		df['grades'] = results
+		print(df.grades)
+		df = df.sort_values(by=['grades'], ascending=False)
+		return df[['name', 'image', 'pays', 'region', 'appelation','domaine', 'millesime', 'couleur', 'description', 'clean_text']].head(2)
+	except:
+		pass
+
+
+def treat_input(input_df,want,text):
+	try:
+		df_want = get_data(input_df,text,want)
+		result_df = df_want
+		return result_df
+	except:
+		pass
